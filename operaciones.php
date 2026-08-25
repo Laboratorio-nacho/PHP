54d52b2a27c8d1f567f2af2f14e4becab82d6131
Nacho <ignacioperezolavarria@gmail.com>
2026-08-25 15:37:16 -0400

    Create operaciones.php

---
+++ operaciones.php
@@ -0,0 +1,11 @@
+<?php
+// Función para sumar dos números
+function sumar($a, $b) {
+    return $a + $b;
+}
+
+// Función para restar dos números
+function restar($a, $b) {
+    return $a - $b;
+}
+?>
